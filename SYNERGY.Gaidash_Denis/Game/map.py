d1ef49cc9d79caf1d1546abb1c44ec33d9f60877
iDeeGee <denisgaidash@gmail.com>
2024-09-04 21:52:07 +0300

    Game.GaidashDenis(add_map)

--- SYNERGY.Gaidash_Denis/Game/map.py
+++ SYNERGY.Gaidash_Denis/Game/map.py
@@ -1,16 +1,32 @@
 # 0 - field
 # 1 - tree
 # 2 - river
+# 3 - hospital
+# 4 - upgrade shop
 class Map:
     #def GenerateRivers():
 
     #def GenerateForest():
 
-    def PrintMap():
+    def PrintMap(self):
         for row in self.cells:
             for cell in row:
                 if cell == 0:
-                    print("", end="")
+                    print("🟩", end="")
+                elif cell == 1:
+                    print("🌲", end="")
+                elif cell == 2:
+                    print("🌊", end="")
+                elif cell == 3:
+                    print("🏥", end="")
+                elif cell == 4:
+                    print("🏦", end="")
+            print()        
 
     def __init__(self, width, height):
         self.cells = [[0 for i in range(width)] for j in range(height)]
+
+# tmp = Map(10,10)
+# tmp.cells[1][1] = 1
+# tmp.cells[2][2] = 2
+# tmp.PrintMap()        
